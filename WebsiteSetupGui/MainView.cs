419017c045696789232a6f89e8ff3264a1e2fa6f
DimucaTheDev <dima.punchenko@gmail.com>
2025-04-27 22:01:07 +0300

    New app: Bugtracker CLI tool. It will be used to manipulate database, settings, and other stuff

---
+++ WebsiteSetupGui/MainView.cs
@@ -0,0 +1,15 @@
+﻿using Terminal.Gui;
+
+namespace WebsiteSetupGui;
+public partial class MainView
+{
+    public MainView()
+    {
+        InitializeComponent();
+        quit.Clicked += () =>
+        {
+            Application.RequestStop();
+        };
+    }
+}
+
---
+++ WebsiteSetupGui/Program.cs
@@ -0,0 +1,25 @@
+﻿using Terminal.Gui;
+
+namespace WebsiteSetupGui;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        if (!args.Any())
+        {
+            try
+            {
+                Console.WriteLine("Starting in TUI mode... Use --cli to use CLI instead.");
+                Application.Init();
+                Application.Run(new MainView());
+            }
+            finally
+            {
+                Application.Shutdown();
+            }
+            return;
+        }
+
+        Console.WriteLine("TODO");
+    }
+}
